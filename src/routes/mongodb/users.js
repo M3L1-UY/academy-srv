934f0eb787cf1a5492f044bdf3a21b747378207c
M3L1-UY <melinaaguiar.uy@gmail.com>
2024-06-21 20:07:55 -0300

    final2

--- src/routes/mongodb/users.js
+++ src/routes/mongodb/users.js
@@ -17,7 +17,7 @@ router.get("/users",isAuthenticated, isAdmin(['isAdmin']), getUsers);
 router.get("/user/:id", isAuthenticated,  isAdmin(['isAdmin']), getUser);
 router.post("/user", isAuthenticated, isAdmin(['isAdmin']), AddUser);
 router.post("/user/login", loginUser);
-router.post("/user/logout", isAuthenticated, logoutUser);
+router.post("/user/logout", logoutUser);
 router.delete("/user/:id",  isAuthenticated, isAdmin(['isAdmin']),delUser);
 router.put(
   "/user/cambio",
